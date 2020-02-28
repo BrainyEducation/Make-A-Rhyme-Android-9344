b5cd47f80bb3e12b58697e21bb08f3a1d6af6258
JonathanQuang <jquang1000@gmail.com>
2020-02-28 02:00:30 -0500

    Added: -subfeature: can add more emails to the email list.     -Error checking for duplicate emails, invalid emails, and going over     max email limit -subfeature: can remove emails from the email list.

--- app/src/main/java/com/example/rhyme_or_reason/brainymake_a_rhyme/EmailActivity.java
+++ app/src/main/java/com/example/rhyme_or_reason/brainymake_a_rhyme/EmailActivity.java
@@ -3,21 +3,45 @@ package com.example.rhyme_or_reason.brainymake_a_rhyme;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.Button;
 import android.widget.PopupWindow;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
 import com.example.rhyme_or_reason.brainymake_a_rhyme.emailSystem.EmailSystem;
 
+import java.util.ArrayList;
+import java.util.HashSet;
+
 public class EmailActivity extends AppCompatActivity implements View.OnClickListener{
+
+    TextInputLayout textInputLayout;
+    TableRow tableRowTemplate;
+    ArrayList<String> emails;
+    EmailSystem emailSystem;
+    TableLayout emailTable;
+    HashSet<String> emailHashSet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_email);
+        textInputLayout = (TextInputLayout) findViewById(R.id.EmailInput);
+        tableRowTemplate  = (TableRow) findViewById(R.id.EmailTableRowTemplate);
+        emailTable = (TableLayout) findViewById(R.id.EmailTable);
+        emails = new ArrayList<String>();
+        emailHashSet = new HashSet<>();
+        emailSystem = new EmailSystem();
+        tableRowTemplate.setVisibility(View.GONE);
     }
 
 
@@ -26,7 +50,7 @@ public class EmailActivity extends AppCompatActivity implements View.OnClickList
         a[0] = "jqdude@gmail.com";
         String subject = "hi";
         String body = "this is the body text";
-        EmailSystem emailSystem = new EmailSystem(a, subject, body);
+        emailSystem.setEmailVariables( emails.toArray(new String[0]), subject, body);
         Intent in = emailSystem.createEmailIntent();
         try {
             startActivity(Intent.createChooser(in, "Send mail..."));
@@ -35,13 +59,69 @@ public class EmailActivity extends AppCompatActivity implements View.OnClickList
         }
     }
 
-    public void configureEmailSetting() {
-        DialogFragment configureEmailFragment = new DialogFragment();
+    public void addEmail(View v) {
+        final String email = textInputLayout.getEditText().getText().toString();
+        if (!emailSystem.isValidEmail(email)) {
+            displayError("Invalid email");
+            return;
+        }
+        if (emailSystem.tooManyEmails(emails)) {
+            displayError("Max emails exceeded");
+            return;
+        }
+        if (emailHashSet.contains(email)) {
+            displayError("Email already exists");
+            return;
+        }
+
+
+        emails.add(email);
+        emailHashSet.add(email);
+
+        TextView exampleEmailTextView = (TextView) tableRowTemplate.getChildAt(0);
+        TextView exampleEmailRemovalButton = (TextView) tableRowTemplate.getChildAt(1);
+
+        TableRow newEmailRow = new TableRow(this);
+        TextView emailTextView = new TextView(this);
+        Button emailRemovalButton = new Button(this);
+        emailTextView.setLayoutParams(exampleEmailTextView.getLayoutParams());
+        emailRemovalButton.setLayoutParams(exampleEmailRemovalButton.getLayoutParams());
+
+        emailTextView.setText(email);
+        emailRemovalButton.setText(exampleEmailRemovalButton.getText());
+        emailRemovalButton.setOnClickListener(generateClickListener(email));
+
+        newEmailRow.addView(emailTextView);
+        newEmailRow.addView(emailRemovalButton);
+        emailTable.addView(newEmailRow);
+
     }
 
+
+
+
+    private void displayError(String s) {
+        Snackbar.make(emailTable, s, Snackbar.LENGTH_LONG).show();
+    }
+
+    private View.OnClickListener generateClickListener(final String email) {
+        View.OnClickListener  returnListener = new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                int rowNum = emails.indexOf(email);
+                emails.remove(rowNum);
+                emailTable.removeViewAt(rowNum+1);
+                emailHashSet.remove(email);
+            }
+        };
+        return returnListener;
+    }
+
+
+
+    @Override
     public void onClick(View v) {
 
     }
-
-
 }
--- app/src/main/java/com/example/rhyme_or_reason/brainymake_a_rhyme/emailSystem/EmailSystem.java
+++ app/src/main/java/com/example/rhyme_or_reason/brainymake_a_rhyme/emailSystem/EmailSystem.java
@@ -3,18 +3,27 @@ package com.example.rhyme_or_reason.brainymake_a_rhyme.emailSystem;
 import android.content.Intent;
 import android.util.Log;
 
+import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 
 public class EmailSystem
 {
     private String[] emails;
     private String subjectLine;
     private String emailBody;
+    public static int maxEmailDisplayLength = 15;
+    //regex comes from https://howtodoinjava.com/regex/java-regex-validate-email-address/
+    private String emailValidationRegex = "^[a-zA-Z0-9_!#$%&’*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$";
+    private Pattern emailRegexPattern;
+    private static int maxEmails = 20;
 
     public EmailSystem() {
-
+        emailRegexPattern = Pattern.compile(emailValidationRegex);
     }
 
-    public EmailSystem(String[] emails, String subjectLine, String emailBody) {
+    public void setEmailVariables(String[] emails, String subjectLine, String emailBody) {
         this.emails = emails;
         this.subjectLine = subjectLine;
         this.emailBody = emailBody;
@@ -27,5 +36,13 @@ public class EmailSystem
         it.putExtra(Intent.EXTRA_TEXT,emailBody);
         it.setType("message/rfc822");
         return it;
+    }
+
+    public boolean isValidEmail(String email) {
+        return emailRegexPattern.matcher(email).matches();
+    }
+
+    public boolean tooManyEmails(ArrayList<String> emails) {
+        return emails.size() > maxEmails;
     }
 }
